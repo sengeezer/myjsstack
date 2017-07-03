a6bf1fe45ccb35adf2bc20205c70eccc3c7b7e40
Fabio Albertin <albertin@gmail.com>
2017-07-03 23:07:41 +0100

    Added test code, babel and eslint

---
+++ src/chicken.js
@@ -0,0 +1,11 @@
+class Chicken {
+  constructor(name) {
+    this.name = name
+  }
+
+  cluck() {
+    return `Hiya, I am ${this.name}`;
+  }
+}
+
+export default Chicken;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Chicken from './chicken';
+
+const daisy = new Chicken('Daisy');
+
+console.log(daisy.cluck());
